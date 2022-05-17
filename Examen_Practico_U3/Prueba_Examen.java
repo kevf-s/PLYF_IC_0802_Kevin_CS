a103b6b2a0a9e05355c3471dd629c1f41a6c89ab
Kevin-CS <73202338+kevf-s@users.noreply.github.com>
2022-05-17 08:04:39 -0500

    Add files via upload

---
+++ Examen_Practico_U3/Prueba_Examen.java
@@ -0,0 +1,12 @@
+public interface Prueba_Examen{
+	public static void main(String[] args) {
+		
+		System.out.println.Prueba_Examen("Hola mi nombre es kevin");
+		public hola= String((String)("la cadena es "));
+		public static void operacion_prueba= a+b= return(int);
+
+		operacion_prueba.Prueba_Examen=(a+b);
+		operacion_prueba.Prueba_Examen=return(a+b);
+	}
+}
+
